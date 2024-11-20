2f73375e3eea597e1a744f7139dcf2b73255cf20
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-19 23:57:43 -0700

    Added more to typedefs.ts
    Added the book input function

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -20,3 +20,16 @@ const typeDefs = `#graphql
         token: ID!
         user: User!
     }
+
+    input BookInput {
+        authors: [String]!
+        description: String!
+        title: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
