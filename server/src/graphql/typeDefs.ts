7f40b57ace57df3859d3de87ff1d50e339024aa2
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-19 23:45:06 -0700

    Added code to the typedefs
    Added code to the typedefs

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -2,9 +2,21 @@ const typeDefs = `#graphql
     type User {
         id: ID!
         email: String!
-        username: string!
+        username: String!
         bookCount: Int!
         savedBooks: [Book]!
     }
 
-    type Book
+    type Book {
+        bookId: String!
+        authors: [String]!
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User!
+    }
